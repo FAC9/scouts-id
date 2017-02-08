02c32593967beb51d6e67d5933e67c2c7a3c2a08
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-08 12:26:10 +0000

    rename handlers file to routes, relatesto issue #11 and PR #13

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-const { homeRoute } = require('./handlers/home.js');
-const { staticRoute } = require('./handlers/static.js');
-const { confirmationRoute } = require('./handlers/confirmation.js');
+const { homeRoute } = require('./routes/home.js');
+const { staticRoute } = require('./routes/static.js');
+const { confirmationRoute } = require('./routes/confirmation.js');
 
 const routes = [
   confirmationRoute,
