4c466e46764809946047cecb8c653ee05c008ebe
John Whiles <johnwhiles@outlook.com>
2017-02-08 12:40:19 +0000

    Add a simple object to hold users, check yoti user ids against scouty status #4

--- src/routes/confirmation.js
+++ src/routes/confirmation.js
@@ -6,6 +6,13 @@ const sdkId = "06f10391-1f16-4e1c-af6c-d6cd6a8714af";
 const yotiPem = Fs.readFileSync(Path.join(__dirname, '../keys/yoti.pem'));
 const yotiClient = new Yoti(sdkId, yotiPem);
 
+const users = {
+  'fAUhmIxQAMxKd7pQ0mACongkiz3OpS1oqLfHpamEN7QT0CCuBy4DCsgzpUn9g87G': {
+    name: 'John Whiles',
+    status: true
+  }
+}
+
 const confirmationHandler = (request, reply) => {
   const token = request.query.token;
   if(!token) {
@@ -15,8 +22,11 @@ const confirmationHandler = (request, reply) => {
   const promise = yotiClient.getActivityDetails(token);
   promise.then((activityDetails) => {
     const userId = activityDetails.getUserId();
-    const profile = activityDetails.getUserProfile();
-    reply(profile);
+    if (users[userId].status === true) {
+      reply('they are a good scout');
+    } else {
+      reply('not a good scout');
+    }
   })
 };
 
