64c7a30c3358696a50c938dbf726ce66cf376803
John Whiles <johnwhiles@outlook.com>
2017-02-10 10:53:42 +0000

    fix styling so background image covers the whole width of the page

--- public/browser.js
+++ public/browser.js
@@ -1,23 +1,16 @@
-// Button to generate QRCode
-var qrbutton = document.querySelector('.get-qr-button');
-// QR Code container element
 var qrcode = document.querySelector('.qr-code-output');
-// Profile name element
 var result = document.querySelector('.result');
-// Profile name element
 
-// 1. On click, call route to get QRCode svg
-qrbutton.addEventListener('click', function () {
+window.onload = function () {
   var o = new XMLHttpRequest();
   o.addEventListener('load', function (e) {
     var responseObj = JSON.parse(e.target.responseText);
-    qrbutton.style.display = 'none';
     qrcode.innerHTML = responseObj.svg;
     listenForToken(responseObj.proto, responseObj.url);
   });
   o.open('GET', '/qr');
   o.send();
-});
+}
 
 // 2. After SVG received, set up WebSocket with YOTI to handle response
 function listenForToken (proto, url) {
